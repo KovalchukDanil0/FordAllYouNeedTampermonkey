3b6cb2f3825bdbd5ba30f5c63b751266f974adfc
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2023-07-02 19:33:26 +0200

    t

--- AEMAPI.js
+++ AEMAPI.js
@@ -1,16 +1,24 @@
-class Rectangle {
+const regexWorkflow =
+  /(?:.+)?wwwperf\.brandeuauthorlb\.ford\.com(?:\/(?:cf#|editor\.html))?\/etc\/workflow\/packages\/ESM\/\w\w(?:\/\w\w\w\w)?\/(.+)\.html(?:.+)?/gm;
+
+class AEM {
   constructor(height, width) {
     this.height = height;
     this.width = width;
   }
-  // Getter
+
+  static get rwf() {
+    return regexWorkflow;
+  }
+
   get area() {
     return this.calcArea();
   }
-  // Method
+
   calcArea() {
     return this.height * this.width;
   }
+
   *getSides() {
     yield this.height;
     yield this.width;
