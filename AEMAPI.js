7c26b32fbd4beb10a9c0e823155cb1d874fd8bc1
KovalchukDanil0 <105009706+KovalchukDanil0@users.noreply.github.com>
2023-07-02 19:14:46 +0200

    t

--- AEMAPI.js
+++ AEMAPI.js
@@ -1,5 +1,20 @@
-class GG {
-  test() {
-    alert("test");
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  // Getter
+  get area() {
+    return this.calcArea();
+  }
+  // Method
+  calcArea() {
+    return this.height * this.width;
+  }
+  *getSides() {
+    yield this.height;
+    yield this.width;
+    yield this.height;
+    yield this.width;
   }
 }
